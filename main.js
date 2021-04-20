af923252d2f1b948daa68d96c21de42397264532
jienhuagoo <mypstom@gmail.com>
2021-04-20 20:35:45 +0800

    [Add] ar.html

--- main.js
+++ main.js
@@ -43,11 +43,36 @@ function init_threeScene(spec) {
   const threeStuffs = JeelizThreeHelper.init(spec, detect_callback);
 
   // CREATE A CUBE
-  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const cubeMaterial = new THREE.MeshNormalMaterial();
-  const threeCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  threeCube.frustumCulled = false;
-  threeStuffs.faceObject.add(threeCube);
+  // const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  // const cubeMaterial = new THREE.MeshNormalMaterial();
+  // const threeCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  // threeCube.frustumCulled = false;
+  // threeStuffs.faceObject.add(threeCube);
+  // threeCube.position.set(0, 1, 0);
+
+  const light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+  threeStuffs.faceObject.add( light );
+
+  const loader = new THREE.GLTFLoader();
+  loader.load(
+    // resource URL
+    './dna.glb',
+    // called when the resource is loaded
+    function ( gltf ) {
+      threeStuffs.faceObject.add( gltf.scene );
+      gltf.scene.scale.set(10, 10, 10);
+      gltf.scene.position.set(0, 0.35, 0);
+      window.dna = gltf.scene;
+    },
+    function ( xhr ) {
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    function ( error ) {
+      console.log( 'An error happened' );
+    }
+  );
+
+
 
   //CREATE THE CAMERA
   THREECAMERA = JeelizThreeHelper.create_camera();
@@ -146,6 +171,9 @@ async function perform() {
     show(document.getElementById("ui_controlls"));
   }
   rendered = true;
+  // if(window.dna){
+  //   window.dna.rotation.y += 0.05;
+  // }
 }
 
 function colorReplace(data) {
