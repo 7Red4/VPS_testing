1a7c99bf24d8bc4602941837fe1042b084dc9d6b
7red4 <abc401344@gmail.com>
2021-05-05 14:44:50 +0800

    feat: update upload info

--- s3/upload.js
+++ s3/upload.js
@@ -1,15 +1,35 @@
 const S3 = require("aws-s3");
 
-const s3config = {
+const s3configResults = {
   bucketName: "tfvirtualphoto",
-  dirName: "customers",
+  dirName: "Customers/Results",
   region: "us-east-2",
   accessKeyId: "AKIA4JTIAKNMJPDZFS7U",
   secretAccessKey: "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",
   s3Url: "https://tfvirtualphoto.s3.us-east-2.amazonaws.com",
 };
 
-window.S3Client = new S3(s3config);
+const s3configUnknown = {
+  bucketName: "tfvirtualphoto",
+  dirName: "Customers/Results",
+  region: "us-east-2",
+  accessKeyId: "AKIA4JTIAKNMJPDZFS7U",
+  secretAccessKey: "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",
+  s3Url: "https://tfvirtualphoto.s3.us-east-2.amazonaws.com",
+};
+
+/**
+  bucketName : tfvirtualphoto
+  dirName : Customers/Results (有揭露)
+  dirName : Customers/Unknown (未揭露) 
+  region : us-east-2
+  accessKeyId : AKIA4JTIAKNMJPDZFS7U
+  secretAccessKey : 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
+  s3Url : https://tfvirtualphoto.s3.us-east-2.amazonaws.com
+ */
+
+window.S3ClientResults = new S3(s3configResults);
+window.S3ClientUnknown = new S3(s3configUnknown);
 
 /**
  * {
@@ -20,10 +40,3 @@ window.S3Client = new S3(s3config);
  *   }
  * }
  */
-window.uploadS3 = async function(image, fileName) {
-  try {
-    return await window.S3Client.uploadFile(image, fileName);
-  } catch (err) {
-    console.error(err);
-  }
-}
